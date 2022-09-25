494ce247009339d95768560bcea146d091091403
Shailesh Dhokare <shailesh19sd@gmail.com>
2022-09-25 23:19:37 +0530

    Dev to main (#4)
    * Created GraphQL schema with sample data
    
    * added client mutations
    
    * completed server functionalities
    
    * Update dev branch (#3)
    
    * node express graphql backend (#1)
    
    * Created GraphQL schema with sample data
    
    * added client mutations
    
    * completed server functionalities
    
    * Dev to main (#2)
    
    * Created GraphQL schema with sample data
    
    * added client mutations
    
    * completed server functionalities
    
    * Added React Client

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const colors = require("colors");
-require("dotenv").config();
-const schema = require("./schema/schema");
-const connectDB = require("./config/db");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 
@@ -11,11 +12,13 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
+
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphiql: process.env.NODE_ENV === 'development',
   })
 );
 
